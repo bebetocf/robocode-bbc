8bed44e4f68c1d4f7a2099b1b1f35f8537c2d418
Roberto Fernandes <betocostaf@gmail.com>
2017-06-07 14:03:20 -0300

    First commit

---
+++ src/main/java/bbc/Helper.java
@@ -0,0 +1,55 @@
+package bbc;
+
+public class Helper {
+
+	// calculate absolute angle from absolute angle y based and relative angle
+    public static double absoluteAngle(double baseAngle, double relativeAngle) {
+        double angle = (baseAngle + relativeAngle) % 360;
+
+        if (angle < 0) {
+            angle += 360;
+        }
+
+        return angle;
+    }
+
+    // Transforms absolute angle into a relative angle, from an absolute base angle
+    public static double relativeAngle(double baseAngle, double destAngle) {
+        double angle = (destAngle - baseAngle) % 360;
+        if (angle > 180) {
+            angle -= 360;
+        } else if (angle < -180) {
+            angle += 360;
+        }
+
+        return angle;
+    }
+
+    // calculate a point's x from its base y, an angle and a distance
+    public static double calculateX(double xBase, double absoluteAngle, double distance) {
+        double offsetX = (Math.sin(Math.toRadians(absoluteAngle)) * distance);
+        return xBase + offsetX;
+    }
+
+    // calculate a point's y from its base x , an angle and a distance
+    public static double calculateY(double yBase, double absoluteAngle, double distance) {
+        double offsetY = (Math.cos(Math.toRadians(absoluteAngle)) * distance);
+        return yBase + offsetY;
+    }
+
+    // calculate the absolute angle between two points (origin and destination)
+    public static double absoluteAngle(double xOrigin, double yOrigin, double xDestination, double yDestination) {
+        double offsetX = xDestination - xOrigin;
+        double offsetY = yDestination - yOrigin;
+
+        return Math.toDegrees(Math.atan2(offsetX, offsetY));
+    }
+
+    // calculate the distance between two points
+    public static double distance(double xOrigin, double yOrigin, double xDestination, double yDestination) {
+        double offsetX = xDestination - xOrigin;
+        double offsetY = yDestination - yOrigin;
+
+        return Math.sqrt(offsetX*offsetX + offsetY*offsetY);
+    }    
+}
---
+++ src/main/java/bbc/Marreta.java
@@ -0,0 +1,186 @@
+package bbc;
+
+import java.util.Vector;
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.rule.FactHandle;
+import org.drools.runtime.rule.QueryResultsRow;
+import robocode.*;
+
+
+public class Marreta extends AdvancedRobot {
+
+    public static String RULES_FILE = "bbc/rules/robot_rules.drl";
+    public static String CONSULT_ACTIONS = "consult_actions";
+    
+    // KBUILDER : knowledge builder. Takes a .drl file in INPUT and OUTPUTS a knowledge package that the kbase can handle
+    private KnowledgeBuilder kbuilder;
+    
+    // KBASE : knowledge base. Contains rules, processes, functions and types models
+    // Does not contain runtime infos.
+    private KnowledgeBase kbase;
+    
+    // KSESSION : class used to dialog with the rules engine
+    private StatefulKnowledgeSession ksession;
+    
+    
+    private Vector<FactHandle> currentReferencedFacts = new Vector<FactHandle>();
+
+    
+    public Marreta(){
+    	// Nothing in constructor
+    }
+    
+
+    public void run() {
+    	DEBUG.enableDebugMode(System.getProperty("robot.debug", "true").equals("true"));
+
+    	// Creates a knowledge base
+    	createKnowledgeBase();
+    	
+        // COLORS
+        setBodyColor(Color.blue);
+        setGunColor(Color.white);
+        setRadarColor(Color.red);
+        setScanColor(Color.white);
+        setBulletColor(Color.red);
+
+        // Make any movement from tank, radar or gun independent
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+
+
+        while (true) {
+//        	turnRadarRight(360);
+        	DEBUG.message("TURN BEGINS");
+            loadRobotState();// HEHRHUHDUHDHUDHUDUHDSHUSUH
+            loadBattleState();
+
+            // Fire rules
+            DEBUG.message("Facts in active memory");
+            DEBUG.printFacts(ksession);           
+            ksession.fireAllRules();
+            cleanAnteriorFacts();
+
+            // Get Actions
+            Vector<Action> actions = loadActions();
+            DEBUG.message("Resulting Actions");
+            DEBUG.printActions(actions);
+
+            // Execute Actions
+            executeActions(actions);
+        	DEBUG.message("TURN ENDS\n");
+            execute();  // Informs the robocode that the turn ended (blocking call)
+
+        }
+
+    }
+
+
+    private void createKnowledgeBase() {
+        String rulesFile = System.getProperty("robot.rules", Marreta.RULES_FILE);
+
+        DEBUG.message("Creating knowledge base");
+        kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        
+        DEBUG.message("Loading rules since "+rulesFile);
+        kbuilder.add(ResourceFactory.newClassPathResource(rulesFile, Marreta.class), ResourceType.DRL);
+        if (kbuilder.hasErrors()) {
+            System.err.println(kbuilder.getErrors().toString());
+        }
+
+        kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        
+        DEBUG.message("Creating session)");
+        ksession = kbase.newStatefulKnowledgeSession();
+    }
+
+
+
+    private void loadRobotState() {
+    	RobotState robotState = new RobotState(this);
+    	currentReferencedFacts.add(ksession.insert(robotState));
+    }
+
+    private void loadBattleState() {
+        BattleState battleState =
+                new BattleState(
+                		getBattleFieldWidth(), 
+                		getBattleFieldHeight(),
+                		getNumRounds(), 
+                		getRoundNum(),
+                		getTime(),
+                		getOthers()
+        );
+        currentReferencedFacts.add(ksession.insert(battleState));
+    }
+
+    private void cleanAnteriorFacts() {
+        for (FactHandle referencedFact : this.currentReferencedFacts) {
+            ksession.retract(referencedFact);
+        }
+        this.currentReferencedFacts.clear();
+    }
+
+    private Vector<Action> loadActions() {
+        Action action;
+        Vector<Action> actionsList = new Vector<Action>();
+
+        for (QueryResultsRow result : ksession.getQueryResults(Marreta.CONSULT_ACTIONS)) {
+            action = (Action) result.get("action");  			// get the Action object
+            action.setRobot(this);                      		// link it to the current robot
+            actionsList.add(action);
+            ksession.retract(result.getFactHandle("action")); 	// clears the fact from the active memory
+        }
+
+        return actionsList;
+    }
+
+    private void executeActions(Vector<Action> actions) {
+        for (Action action : actions) {
+            action.initExecution();
+        }
+    }
+
+    // Insert in memory the different happening events
+    public void onBulletHit(BulletHitEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onBulletHitBullet(BulletHitBulletEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onBulletMissed(BulletMissedEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onHitByBullet(HitByBulletEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onHitRobot(HitRobotEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onHitWall(HitWallEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onRobotDeath(RobotDeathEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+
+    public void onScannedRobot(ScannedRobotEvent event) {
+    	currentReferencedFacts.add(ksession.insert(event));
+    }
+}
