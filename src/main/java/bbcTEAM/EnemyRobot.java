847398f705d2c3d41b1d788bfddab91ee0cbff41
ccal1 <ccal@cin.ufpe.br>
2017-06-30 16:16:43 -0300

    updated enemy robot to be the same as the in the individual teams

--- src/main/java/bbcTEAM/EnemyRobot.java
+++ src/main/java/bbcTEAM/EnemyRobot.java
@@ -1,5 +1,9 @@
 package bbcTEAM;
 import java.io.Serializable;
+
+import bbc.Helper;
+import bbc.RobotState;
+
 import java.awt.geom.*;
 
 import robocode.*;
@@ -8,17 +12,14 @@ public class EnemyRobot extends Point2D.Double{
 	double x, y, bearing, energy, velocity, heading, distance;
 	String name;
 	long time;
-	
-	public EnemyRobot(ScannedRobotEvent event){
-		this.name = event.getName();
-		this.bearing = event.getBearing();
-		this.distance = event.getDistance();
-		this.energy = event.getEnergy();
-		this.heading = event.getHeading();
-		this.velocity = event.getVelocity();
-		this.time=0;
+	boolean leader;
+	public void setLeader(boolean leader) {
+		this.leader = leader;
 	}
-	
+	public boolean isLeader() {
+		return leader;
+	}
+
 	public long getTime() {
 		return time;
 	}
@@ -55,11 +56,49 @@ public class EnemyRobot extends Point2D.Double{
 	public String getName() {
 		return name;
 	}
+	public void setDistance(double x, double y)
+	{
+		this.distance = Helper.distance(this.x,this.y,x,y);
+	}
+	public double getFirePower()
+	{
+		return Helper.firePower(this.distance);
+	}
+	public double getBulletSpeed()
+	{
+		return Helper.bulletSpeed(Helper.firePower(this.distance));
+	}
+	public long getTimeToTarget()
+	{
+		return Helper.time(this.distance, this.getBulletSpeed());
+	}
 	
 	public EnemyRobot() {
 		reset();
 	}
 
+	public EnemyRobot(ScannedRobotEvent event) {
+		this.update(event);
+		if(event.getEnergy()<=100)this.leader=true;
+		else this.leader=false;
+	}
+	
+	public EnemyRobot(ScannedRobotEvent event, RobotState state) {
+		this.update(event, state);
+		if(event.getEnergy()<=100)this.leader=true;
+		else this.leader=false;
+	}
+	
+	public void update(ScannedRobotEvent event, RobotState state) {
+		this.update(event);
+
+		//double absAngle = (state.getHeading() + event.getBearing());
+		//if (absAngle < 0) absAngle += 360;
+		this.x = Helper.enemyX(state.getX(), state.getHeading(), event.getBearing(), event.getDistance());
+		this.y = Helper.enemyY(state.getY(), state.getHeading(), event.getBearing(), event.getDistance());
+		//System.out.println("Enemy position:" + this.getX() + " " + this.getY());
+	}
+	
 	public boolean noEnemy() {
 		if (this.name.equals("")) {
 			return true;
@@ -71,11 +110,23 @@ public class EnemyRobot extends Point2D.Double{
 	public double getFutureX(long _time){
 		return this.x + Math.sin(Math.toRadians(this.heading)) * this.velocity * _time;
 	}
-
+	public double getFutureX(){
+		return Helper.enemyFutureX(this.getX(), this.getHeading(), this.getVelocity(), this.getTimeToTarget());
+	}
+	
 	public double getFutureY(long _time){
 		return this.y + Math.cos(Math.toRadians(this.heading)) * this.velocity * _time;
 	}
-	
+	public double getFutureY(){
+		return Helper.enemyFutureY(this.getY(), this.getHeading(), this.getVelocity(), this.getTimeToTarget());
+	}
+	public boolean isUpdated(long time){
+		return time>(16 + this.time);
+	}
+	public void print()
+	{
+		System.out.println("Name : " + this.getName() + " time:"+ this.getTime());
+	}
 	public void update(ScannedRobotEvent event){
 		this.name = event.getName();
 		this.bearing = event.getBearing();
@@ -83,11 +134,10 @@ public class EnemyRobot extends Point2D.Double{
 		this.energy = event.getEnergy();
 		this.heading = event.getHeading();
 		this.velocity = event.getVelocity();
-		this.time=0;
+		this.time=event.getTime();
 	}
 	
 	public void update (ScannedRobotEvent event, Robot robot){
-		setTime(robot.getTime());
 		update(event);
 		double absAngle = (robot.getHeading() + event.getBearing());
 		if (absAngle < 0) absAngle += 360;
@@ -105,5 +155,15 @@ public class EnemyRobot extends Point2D.Double{
 		this.x = 0;
 		this.y = 0;
 		this.time = 0;
+		this.leader=false;
+	}
+	public boolean isBetter(EnemyRobot other)
+	{
+		if(other.isLeader() && !this.isLeader())return false;
+		else if(this.isLeader() && !other.isLeader())return true;
+		else
+		{
+			return this.getDistance() < other.getDistance();
+		}
 	}
 }
